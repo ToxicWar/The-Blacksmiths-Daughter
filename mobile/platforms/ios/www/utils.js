71bc7d5516f40b9c9b4a1ced0b225326acffa829
Anton Larkin <toxicwar94@yandex.ru>
2014-03-22 11:08:37 +0400

    Deleted ios project

--- mobile/platforms/ios/www/utils.js
+++
@@ -1,9 +0,0 @@
-function XHR(method, path, data, callback) {
-	var xhr = new XMLHttpRequest();
-	xhr.open(method, path, true);
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState != 4) return;
-		callback(xhr.status, xhr.responseText);
-	}
-	xhr.send(data);
-}
