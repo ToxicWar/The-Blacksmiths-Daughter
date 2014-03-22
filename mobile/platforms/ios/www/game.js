29caab6854ae6c4e153900cc24a48731647f9049
Anton Larkin <toxicwar94@yandex.ru>
2014-03-22 06:06:51 +0400

    Updated mobile platform

---
+++ mobile/platforms/ios/www/game.js
@@ -0,0 +1,11 @@
+$(function() {
+	var ctx = document.getElementById('canvas').getContext('2d');
+	var scale = 38;
+
+	ctx.fillStyle = "rgba(0, 0, 200)";
+	for (var i = 0; i < 10; i++) {
+		for (var j = 0; j < 10; j++) {
+			ctx.fillRect (i*(scale+2), j*(scale+2), scale, scale);
+		}
+	}
+});
