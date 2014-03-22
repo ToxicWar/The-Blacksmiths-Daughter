8364849c4331caaaac0973c6f228fae267b37a78
Anton Larkin <toxicwar94@yandex.ru>
2014-03-22 11:27:14 +0400

    Updated tizen config.js

--- tizen/config.js
+++ tizen/config.js
@@ -1,15 +1,15 @@
 var serverConf = {
 //	addr: "10.0.3.241",
 //	port: 5000
-	addr: location.hostname,
-	port: location.port
+	addr: '192.168.137.174',
+	port: 5000
 };
 
 
 var urlParams = location.hash.substr(1).split(",");
 var pupsConf = {
-	w: urlParams[0] || 10,
-	h: urlParams[1] || 8,
-	cw: urlParams[2] || 78,
-	iw: urlParams[3] || 70
+	w: urlParams[0] || 7,
+	h: urlParams[1] || 13,
+	cw: urlParams[2] || 48,
+	iw: urlParams[3] || 40
 };
