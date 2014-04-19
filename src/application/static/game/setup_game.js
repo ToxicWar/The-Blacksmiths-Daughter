2ebce91679382d036ee97d346053c673b8370980
3bl3gamer <3bl3gamer@gmail.com>
2014-04-19 16:07:03 +0000

    Partially working

--- src/application/static/game/setup_game.js
+++ src/application/static/game/setup_game.js
@@ -2,32 +2,37 @@ core.on("map-turn-done", function(i, j, color) {
 	console.log("turn done, color: with #:", color.toString(), "as int:", color.valueOf());
 });
 
+function getMapRandomGenerators() {
+	return [
+		MapGenerator.cellRandom,
+		MapGenerator.dirMiddleSnake,
+		MapGenerator.wallBorder,
+		[MapGenerator.hole, true, 0.75, 0.25, 0.15],
+		MapGenerator.playersPositions
+	];
+}
+
+function getGenerators(mapData) {
+	if (gameConf.multiplayer || !mapData) {
+		return getMapRandomGenerators()
+	} else {
+		return [
+			[MapGenerator.unpackGrid, mapData]
+		];
+	}
+}
+
+function getPlayersColors() {
+	return [Color.GREEN, Color.RED];
+}
+
 function setupMap(mapData, playerColor) {
 	var map = new Map({
 		canvas: theGameCanvas,
 		h_size: pupsConf.w,
 		v_size: pupsConf.h,
-		cell_width: pupsConf.cw,
-		generators: (!gameConf.multiplayer ? [
-			MapGenerator.cellRandom,
-			MapGenerator.dirMiddleSnake,
-			MapGenerator.wallBorder,
-			[MapGenerator.hole, true, 0.75, 0.25, 0.15],
-			MapGenerator.playersPositions
-		] : (!mapData ? [
-			MapGenerator.cellRandom,
-			MapGenerator.wallBorder,
-			[MapGenerator.hole, true, 0.5, 0.5, 0.15]
-		] : [
-			[MapGenerator.unpackGrid, mapData],
-		])),
-		playersColors: [Color.GREEN, Color.RED],
-		onTurn: function(i, j, color) {
-			core.emit("map-turn-done", [i, j, color]); // TODO: если труъ, надо будет впилить в саму карту
-		},
-		onAnimationEnd: function(color) {
-			core.emit("map-animation-end", [color]);
-		}
+		cell_width: getGenerators(mapData),
+		playersColors: getPlayersColors(),
 	});
 	
 	playerColor = playerColor || Color.GREEN;
@@ -115,86 +120,15 @@ function setupMap(mapData, playerColor) {
 
 
 core.on("window-onload", function() {
-	if (!gameConf.multiplayer) {
-		var map = setupMap();
-		var bots = [new TestAi(map, Color.RED, function() {
-			alert("Ах ты такой разэдакий! Давай ещё раз.");
-		})];
-		core.on("map-animation-end", function(color) {
-			for (var i=0; i<bots.length; i++) {
-				if (bots[i].color.valueOf() != color.valueOf()) continue;
-				bots[i].turn(map);
-			}
-		});
-		return;
-	}
-	
-	var room = "someroom";
-	var positionsForRoommates;
-	var socket = io.connect('http://' + serverConf.addr + ':' + serverConf.port + '/test');
-	
-//	socket.emit('hello to roommates', {room: room});
-//	
-//	socket.on('hello from roommates', function(msg) {
-//		console.log(msg)
-//		if (msg.rooms.indexOf(room) == -1) {
-//			setupMap();
-//			socket.emit('map to roommates', {room: room, map: map.packGrig()});
-//		}
-//	});
-//	
-//	socket.on('map from roommates', function(msg) {
-//		console.log(msg)
-//		map.applyGenerator([
-//			MapGenerator.playersPositions,
-//			function(positions){ positionsForRoommates = positions }
-//		]);
-//	});
-	
-	socket.emit('get multiplayer event', {});
-	
-	core.on("map-turn-done", function(i, j, color) {
-		if (gameConf.multiplayer) {
-			socket.emit('turn event', {
-				pos: {i:i, j:j}
-			});
+	var map = setupMap();
+	var bots = [new TestAi(map, Color.RED, function() {
+		alert("Ах ты такой разэдакий! Давай ещё раз.");
+	})];
+	core.on("map-animation-end", function(color) {
+		for (var i=0; i<bots.length; i++) {
+			if (bots[i].color.valueOf() != color.valueOf()) continue;
+			bots[i].turn(map);
 		}
 	});
-	
-	
-	socket.on('get multiplayer map', function(msg) {
-		console.log(msg)
-		if (msg.map.length == 0) {console.log("len 0")
-			var map = setupMap();
-			console.log("apply")
-			map.applyGenerator([
-				MapGenerator.playersPositions,
-				function(positions) {
-					map.drawAll();
-					console.log("p", positions)
-					for (var i=0; i<positions.length; i++) {
-						positions[i].col = positions[i].col.toString()
-					}
-					socket.emit('set multiplayer map', {
-						map: {
-							data: map.packGrig(),
-							positions: positions
-						}
-					});
-				}
-			]);
-		} else {
-			var map = setupMap(msg.map.data, Color.RED);
-			console.log("mmp", msg.map.positions)
-			for (var i=0; i<msg.map.positions.length; i++) {
-				var pos = msg.map.positions[i];
-				map.hackColor(pos.i, pos.j, Color.fromHTML(pos.col));
-			}
-		}
-	});
-
-	socket.on('turn event', function(msg) {
-		console.log(msg)
-		map.doTurn(msg.pos.i, msg.pos.j, 1, map.cellAt(msg.pos.i, msg.pos.j));
-	});
+	// сокеты? а чего, оно всё равно не работает?
 });
